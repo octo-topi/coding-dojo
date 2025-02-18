e3f3c4248c176bd968231c4fcebbb978a9e785fe
Pierre TOP (TOPI) <ptop@octo.com>
2025-02-18 17:05:34 +0100

    First passing test

--- test.js
+++ test.js
@@ -1,5 +1,36 @@
 import { expect } from 'chai'
 
-it('should fail',()=>{
-    expect(true).to.equal(false)
-})
+// input (earth): forward, backward, left, right
+// output (rover):
+// - (rotate) North, South, East, West
+// - forward, backward
+
+// test list
+// - when no command, rover stays in position
+// - when is 0,0 facing North, received forward, rover end in position 0,1 facing North
+
+
+context('given rover is 0,0 facing North',()=>{
+    context('when the command list is empty',()=>{
+        it('should end 0,0 facing North',()=>{
+            // given
+            const rover = new Rover({ x: 0, y:0, direction: 'NORTH'})
+
+            // when
+            rover.command([]);
+
+            // then
+            expect(rover.getPosition()).to.deep.equal({x:0, y:0, direction: 'NORTH'})
+        })
+    })
+});
+
+class Rover {
+    constructor({ x , y, direction}){
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+    command(){}
+    getPosition(){return {x:0, y:0, direction: 'NORTH'}}
+}
