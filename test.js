1c4873f31003db4e8fc9bfa43afde943a11d0670
Pierre TOP (TOPI) <ptop@octo.com>
2025-02-18 17:40:47 +0100

    Add formatter

--- test.js
+++ test.js
@@ -10,28 +10,33 @@ import { expect } from 'chai'
 // - refactor: extract position from direction
 // - when is 0,0 facing North, received forward, rover end in position 0,1 facing North
 
-
-context('given rover is 0,0 facing North',()=>{
-    context('when the command list is empty',()=>{
-        it('should end 0,0 facing North',()=>{
+context('given rover is 0,0 facing North', () => {
+    context('when the command list is empty', () => {
+        it('should end 0,0 facing North', () => {
             // given
-            const rover = new Rover({ x: 0, y:0, direction: 'NORTH'})
+            const rover = new Rover({ x: 0, y: 0, direction: 'NORTH' })
 
             // when
-            rover.command([]);
+            rover.command([])
 
             // then
-            expect(rover.getPosition()).to.deep.equal({x:0, y:0, direction: 'NORTH'})
+            expect(rover.getPosition()).to.deep.equal({
+                x: 0,
+                y: 0,
+                direction: 'NORTH',
+            })
         })
     })
-});
+})
 
 class Rover {
-    constructor({ x , y, direction}){
-        this.x = x;
-        this.y = y;
-        this.direction = direction;
+    constructor({ x, y, direction }) {
+        this.x = x
+        this.y = y
+        this.direction = direction
     }
-    command(){}
-    getPosition(){return {x:0, y:0, direction: 'NORTH'}}
+    command() {}
+    getPosition() {
+        return { x: 0, y: 0, direction: 'NORTH' }
+    }
 }
